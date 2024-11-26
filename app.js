14992f80d47217c63715515b6610513ee27462b8
ARJUN KOLLIPARA <123087955+Arjunkollipara@users.noreply.github.com>
2024-11-26 10:54:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Docker!');
+  res.send('Hello World from the Dockerized Node.js app!');
 });
 
-app.listen(3000, '0.0.0.0', () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
